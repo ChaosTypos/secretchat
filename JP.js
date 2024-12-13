d3c4ddf4b281d8cddffb5f10528b8dd990c6a061
ChaosTypos <chaostypos@gmail.com>
2024-12-13 22:55:39 +0530

    new

---
+++ JP.js
@@ -0,0 +1,13 @@
+git remote add origin https://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@github.com/ChaosTypos/securechat
+
+git push --set-upstream origin master
+
+ git config --global user.name ""
+
+ git config --global user.email ""
+
+ git config --global init.defaultBranch main
+
+ git branch -m main
+
+ git clone https://ChaosTypos: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@github.com/ChaosTypos/secretchat.git
